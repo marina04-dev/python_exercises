3e090b061926c4504880cca896558c60154838ce
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-17 22:59:01 +0200

    Create Numbers Summary2 - while loop.py

---
+++ Numbers Summary2 - while loop.py
@@ -0,0 +1,16 @@
+# Numbers Summary: while loop (list-version)
+i=0
+my_list = []
+summary = 0
+while i<10:
+    num = input("Enter a number: ")
+    if num.isdigit():
+        my_num = int(num)
+        my_list.append(my_num)
+        summary += my_list[i]
+        i += 1 
+    else:
+        print("Wrong Input!")
+        continue
+print(my_list)
+print(summary)
